48df543495bbfbb9f3d5ebcb2710aed8aa9e1fb0
mwt2212 <michaelt3538@outlook.com>
2024-08-01 22:18:10 -0500

    Heuristic function basic implementation, unfinished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# connect4_solver" 
+Started by creating core game logic, functions to create board, make a move, and check for wins.
+Added visualization with pygame.
+
--- src/ai.py
+++ src/ai.py
@@ -26,7 +26,7 @@ def is_terminal_node(board, game):
 
 def minimax(board, depth, alpha, beta, maximizingPlayer, game):
     valid_columns = get_valid_columns(board)
-    is_terminal = is_terminal_node(board, game)
+    is_terminal = is_terminal_node(board, game) 
 
     if depth == 0 or is_terminal:
         if is_terminal:
@@ -37,7 +37,7 @@ def minimax(board, depth, alpha, beta, maximizingPlayer, game):
             else:
                 return (None, 0)
         else:
-            # TODO: Implement Scoring Function
+            # TODO: Implement Scoring/Heuristic Function
             return (None, score_position(board, 1))
 
     if maximizingPlayer:
@@ -69,20 +69,134 @@ def minimax(board, depth, alpha, beta, maximizingPlayer, game):
                 break
         return column, value
     
-
-
-def score_position(board, player):
-    score = 0
-    def two_in_row(board, player):              # Return number of 2 in a row
-        pass
-    def open_three_in_row(board, player):             # Return number of 3 in a row
-        pass
-    def one_ply_three_in_row(board, player):    # Return number of ways to create a three in a row in one move
-        pass                                        # If moving to some pos. creates 3 open spots where a 4 in a row could be acheived, count etc.
+def check_patterns(board, player):
+    rows = len(board)
+    cols = len(board[0])
+    count_two = 0
+    count_three = 0
+    total_pieces = 0
+    used_indices = set()
     
+    for row in range(rows):
+        for col in range(cols):
+
+            if board[row][col] == player:
+                total_pieces += 1
+
+            if col <= cols - 3:                 # Horizontal
+                sequence = board[row][col:col+2]
+                adj1 = board[row][col+2] if col+2 < cols else -1
+                adj2 = board[row][col-1] if col-1 >= 0 else -1
+
+                if sequence.count(player) == 2:
+                    if adj1 == 0:
+                        count_two += 1
+                    if adj2 == 0:
+                        count_two += 1
+
+                if col <= cols - 4:
+                    sequence = board[row][col:col+3]
+                    adj1 = board[row][col+3] if col+3 < cols else - 1
+                    adj2 = board[row][col-1] if col-1 >= 0 else -1
+
+                    if sequence.count(player) == 3:
+                        if adj1 == 0:
+                            count_three += 1
+                        if adj2 == 0:
+                            count_three += 1
+            
+            if row <= rows - 3:                 # Vertical
+                sequence = [board[row+i][col] for i in range(2)]
+                adj1 = board[row+2][col] if row+2 < rows else -1
+
+                if sequence.count(player) == 2 and adj1 == 0:
+                    count_two += 1
+                if row <= rows - 4:
+                    sequence = [board[row+i][col] for i in range(3)]
+                    adj1 = board[row+3][col] if row+3 < rows else -1
+                    if sequence.count(player) == 3 and adj1 == 0:
+                        count_three += 1
+
+            if row <= rows - 3 and col <= cols - 3:     # Pos sloped diagonal
+                sequence = [board[row+i][col+i] for i in range(2)]
+                adj1 = board[row+2][col+2] if row+2 < rows and cols+2 < cols else -1
+                adj2 = board[row-1][col-1] if row-1 >= 0 and col-1 >= 0 else -1
+
+                if sequence.count(player) == 2:
+                    if adj1 == 0:
+                        count_two += 1
+                    if adj2 == 0:
+                        count_two += 1
+                    
+                if rows <= rows - 4 and cols <= cols - 4:
+                    sequence = [board[row+i][col+i] for i in range(3)]
+                    adj1 = board[row+3][col+3] if row+3 < rows and col+3 < cols else -1
+                    adj2 = board[row-1][col-1] if row-1 >= 0 and col-1 >= 0 else -1
+
+                    if sequence.count(player) == 3:
+                        if adj1 == 0:
+                            count_three += 1
+                        if adj2 == 0:
+                            count_three += 1
+            
+            if row >= 2 and col <= cols - 3:            # Neg sloped diagonal
+                sequence = [board[row-i][col+i] for i in range(2)]
+                adj1 = board[row-2][col+2] if row-2 >= 0 and col+2 < cols else -1
+                adj2 = board[row+1][col-1] if row+1 < rows and col-1 >= 0 else -1
+
+                if sequence.count(player) == 2:
+                    if adj1 == 0:
+                        count_two += 1
+                    if adj2 == 0:
+                        count_two += 1
+
+                if row >= 3 and col <= cols - 4:
+                    sequence = [board[row-i][col+i] for i in range(3)]
+                    adj1 = board[row-3][col+3] if row-3 >= 0 and col+3 < cols else -1
+                    adj2 = board[row+1][col-1] if row+1 < rows and col-1 >= 0 else -1
+
+                    if sequence.count(player) == 3:
+                        if adj1 == 0:
+                            count_three += 1
+                        if adj2 == 0:
+                            count_three += 1
+    print(count_two, count_three, total_pieces)
+    return count_two, count_three, total_pieces
+                
+
+def score_position(board, player, weights=None):
+    if weights is None:
+        weights = {
+            'total_pieces': 0.1,
+            'count_two': 0.3,
+            'count_three': 0.9
+        }
+
+    count_two, count_three, total_pieces = check_patterns(board, player)
+
+    score = (total_pieces * weights.get('total_pieces') +
+             count_two * weights.get('count_two') +
+             count_three * weights.get('count_three'))
+
     return score
 
 
 def ai_move(game):
     pass
 
+board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+player = 1
+
+print(score_position(board, player))
+
+#EV: 3 two-inna-rows, + 3 pieces total
+# 3 * 0.3 + 3 * 0.1
+#  0.9 + 0.3 = 1.2
